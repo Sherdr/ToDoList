7683286cdb91587d0952e486ba4a4d19805cb2c7
Sherdr <rainar_kuper@mail.ru>
2024-05-11 22:59:18 +0300

    Add "TaskEntity.cs"

---
+++ ToDoList.Domain/Entity/TaskEntity.cs
@@ -0,0 +1,10 @@
+﻿using ToDoList.Domain.Enum;
+
+namespace ToDoList.Domain.Entity {
+    public class TaskEntity {
+        public long ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Priority Priority { get; set; }
+    }
+}
---
+++ ToDoList.Domain/Enum/Priority.cs
@@ -0,0 +1,7 @@
+﻿namespace ToDoList.Domain.Enum {
+    public enum Priority {
+        Eazy = 1,
+        Medium = 2,
+        Hard = 3
+    }
+}
