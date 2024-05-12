29bd3cf52519a04ad03301a832668a8343c1dad7
Sherdr <rainar_kuper@mail.ru>
2024-05-12 19:22:09 +0300

    Создание запроса для добавления "Task"

--- ToDoList.Domain/Enum/Priority.cs
+++ ToDoList.Domain/Enum/Priority.cs
@@ -1,7 +1,12 @@
-﻿namespace ToDoList.Domain.Enum {
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDoList.Domain.Enum {
     public enum Priority {
+        [Display(Name = "Простая")]
         Eazy = 1,
+        [Display(Name = "Важная")]
         Medium = 2,
+        [Display(Name = "Критичная")]
         Hard = 3
     }
 }
--- ToDoList/Controllers/TaskController.cs
+++ ToDoList/Controllers/TaskController.cs
@@ -1,10 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ToDoList.Domain.ViewModels.Task;
 
 namespace ToDoList.Controllers {
     public class TaskController : Controller {
-
         public IActionResult Index() {
             return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateTaskViewModel model) {
+            return Ok();
         }
     }
 }
